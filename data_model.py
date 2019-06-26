8af685d09896ea779eb6e3c78b9d6c68f4939caa
Administrador <hitalo.emanoel@gmail.com>
2019-06-26 18:25:33 -0300

    data_model created

---
+++ data_model.py
@@ -0,0 +1,8 @@
+
+
+class DataModel():
+    def __init__(self, name, month, is_leapyear, day1):
+        self.name = name
+        self.month = month
+        self.is_leapyear = is_leapyear
+        self.day1 = day1
