2c1f5a5583fbdf35e69698bb54aafb8590784f51
Administrador <hitalo.emanoel@gmail.com>
2019-06-26 18:55:43 -0300

    added default values to data_model

--- data_model.py
+++ data_model.py
@@ -1,8 +1,9 @@
 
 
 class DataModel():
-    def __init__(self, name, month, is_leapyear, day1):
+    def __init__(self, name="", month=0, is_leapyear=0, day1=0):
         self.name = name
         self.month = month
         self.is_leapyear = is_leapyear
         self.day1 = day1
+
