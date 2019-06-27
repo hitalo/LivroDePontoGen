34c3bd4428d688fa4fd06d596594c3f565baaebf
Hitalo Emanoel <hitalo.emanoel@gmail.com>
2019-06-27 19:27:11 -0300

    header added

--- pdf_gen.py
+++ pdf_gen.py
@@ -25,7 +25,7 @@ class PDFGen:
 
             data_table = self.make_table(name, rows, month, vacations, diff)
 
-            t = Table(data_table, 11 * [0.65 * inch], (rows + 1) * [0.35 * inch])
+            t = Table(data_table, 11 * [0.65 * inch], (rows + 2) * [0.33 * inch])
             t.setStyle(TableStyle(self.get_table_style(rows, vacations, diff)))
 
             self.elements.append(t)
@@ -39,6 +39,7 @@ class PDFGen:
     def make_table(self, name, rows, month, vacations, diff):
 
         data_table = [[name]]
+        data_table.append(["Data", 'Entrada', 'Assinatura', '', '', 'Saída', 'Entrada', 'Assinatura', '', '', 'Saída'])
         for i in range(1, rows + 1, 1):
 
             if (i % 7 == diff): # saturday
@@ -60,17 +61,17 @@ class PDFGen:
     def get_table_style(self, rows, vacations, diff):
         # default
         table_style = [
-            ('ALIGN', (0, 0), (10, rows), 'CENTER'),
-            ('VALIGN', (0, 0), (10, rows), 'MIDDLE'),
-            ('BOX', (0, 0), (10, rows), 0.25, colors.black),
-            ('INNERGRID', (0, 0), (10, rows), 0.25, colors.black),
+            ('ALIGN', (0, 0), (10, rows+1), 'CENTER'),
+            ('VALIGN', (0, 0), (10, rows+1), 'MIDDLE'),
+            ('BOX', (0, 0), (10, rows+1), 0.25, colors.black),
+            ('INNERGRID', (0, 0), (10, rows+1), 0.25, colors.black),
             ('SPAN', (0, 0), (10, 0)),
             ('SPAN', (2, 1), (4, 1)),
             ('SPAN', (7, 1), (9, 1))
         ]
 
         # big cells for names
-        for i in range(1, rows+1, 1):
+        for i in range(1, rows+2, 1):
             table_style.append(('SPAN', (2, i), (4, i)))
             table_style.append(('SPAN', (7, i), (9, i)))
 
@@ -78,7 +79,7 @@ class PDFGen:
         # different color for saturdays and sundays
         for i in range(1, rows + 1, 1):
             if (i in vacations or i % 7 == diff  or i % 7 == diff + 1 or (diff == 6 and i % 7 == 0)):
-                table_style.append(('BACKGROUND', (0, i), (11, i), colors.gray))
+                table_style.append(('BACKGROUND', (0, i+1), (11, i+1), colors.gray))
 
         return table_style
 
